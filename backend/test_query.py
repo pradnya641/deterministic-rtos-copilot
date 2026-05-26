89f91ac5b13cc21f9937ae192809eab9ea86286d
pradnya641 <purohitpradnya11@gmail.com>
2026-05-26 19:14:13 +0530

    Delete backend/test_query.py

--- backend/test_query.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://localhost:8000/ask"
-payload = {
-    "text": "Setup ADC for channel 1 on P0.28"
-}
-
-response = requests.post(url, json=payload)
-print(response.json())
